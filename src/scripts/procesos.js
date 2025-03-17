b35e06adb247f95deaca031f076f73a322f696f3
Fidel Nadai <fidelnadai1@outlook.es>
2025-03-16 23:38:02 -0300

    CorreccionesQuePediste

---
+++ src/scripts/procesos.js
@@ -0,0 +1,8 @@
+let currentIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}, 3000); // Cambia de imagen cada 3 segundos
